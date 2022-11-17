53d183c82555e0dd14212e2b49ae7f4f7d2080c5
Harimurti18 <117901226+Harimurti18@users.noreply.github.com>
2022-11-17 16:23:01 +0700

    Add files via upload

---
+++ 2_D_71220918.py
@@ -0,0 +1,10 @@
+alas = float(input("Masukan alas atap : "))
+print (alas)
+tinggi = float(input("Masukan tinggi atap : "))
+print (alas)
+tembok = float(input("Masukan tinggi tembok : "))
+print (alas)
+luas1 = alas*tinggi/2
+luas2 = alas*tembok
+luas3 = luas1 + luas2
+print = ("rumah tersebut membutuhkan "+ str(luas3/5))
